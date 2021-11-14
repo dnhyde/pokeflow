956a2734cc2072b6995045c9188cc9b2ef1c94c2
dnhyde <occhiutodaniele@gmail.com>
2021-11-14 23:32:23 +0100

    Add spotless ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    id("com.diffplug.spotless") version "5.14.3"
+}
+
 buildscript {
     repositories {
         google()
@@ -10,5 +15,24 @@ buildscript {
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
+    }
+}
+
+subprojects {
+
+    apply(plugin = "com.diffplug.spotless")
+    spotless {
+        kotlin {
+            // by default the target is every '.kt' and '.kts` file in the java sourcesets
+            target("**/*.kt")
+            targetExclude("$buildDir/**/*.kt")
+            targetExclude("bin/**/*.kt")
+            ktlint("0.42.1").userData(
+                mapOf("max_line_length" to "120", "disabled_rules" to "import-ordering")
+            )
+            trimTrailingWhitespace()
+            indentWithSpaces()
+            endWithNewline()
+        }
     }
 }
